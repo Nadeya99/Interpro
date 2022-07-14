50af74dc9e1d83eb22a9ac8967c5f1b49e44ed9a
Nadya <rozy93@bk.ru>
2022-07-14 13:05:31 +0600

    func

--- js/script.js
+++ js/script.js
@@ -238,3 +238,113 @@ for (let i = 0; i <= lines; i++) {
 }
 
 console.log(resulti);
+
+function showFirstMessage(text) {
+    console.log(text);
+}
+
+showFirstMessage("Hello World!");
+
+function calc(a, b) {
+    return (a + b);
+}
+console.log(calc(2, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+// Функциональное выражение
+const logger = function() {
+    console.log("Hello World");
+};
+logger();
+
+// Стрелочная функция
+const cilc = (c, d) => {return c + d; };
+// Если в одну строчку, можно так
+const milc = (c, d) =>  c + d;
+
+// function convert(amount) {
+//     console.log(28 * amount);
+// }
+// convert(500);
+
+// const usdCurr = 28;
+// const eurCurr = 32;
+
+// function convert(amount, curr) {
+//     console.log(curr * amount);
+// }
+// convert(500, usdCurr);
+// convert(500, eurCurr);
+
+
+const usdCurr = 28;
+const discount = 0.9;
+
+function convert(amount, curr) {
+    return curr * amount;
+}
+
+function promotion(result){
+    console.log(result * discount);
+}
+
+const res = convert(500, usdCurr);
+promotion(res);
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if(i === 3) return 
+    }
+    console.log('Done');
+}
+
+test();
+
+// Место для первой задачи
+function sayHello(name) {
+    const say = console.log('Привет,' + name + '!');
+    return say;
+}
+
+sayHello("Антон");
+
+// Место для второй задачи
+function returnNeighboringNumbers(num) {
+    const arr = [num - 1, num, num + 1];
+    return arr;   
+}
+console.log(returnNeighboringNumbers(5));
+
+// Место для третьей задачи
+// function getMathResult(num, times) {
+//     for(let i = 0; i < b;) {
+//         let num =console.log(a**b);
+//         console.log(num + '---');
+//     }
+// }
+// getMathResult(2, 3);
+
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
+
+getMathResult(10, 5);
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+function getMathResult(a, b) {
+    for(let i = 0; i < b; i**b) {
+        const res = a**b;
+        console.log(res + '---');
+    }
+}
+
+getMathResult(2, 3);
